6314f50b71fffd5c148efafef6d5789dbf43bf2b
Geo_mzn <geomznplays@gmail.com>
2026-07-18 21:24:53 -0700

    ts ass

--- assets/onlinifier/api.js
+++
@@ -1,2 +0,0 @@
-//made with one line of code (this comment line doesn't count ok)
-function tof(id, mode) {fetch("https://corsproxy.io/?" + encodeURIComponent(`https://bopimo.com/api/users/${id}`), {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify({"online": mode})});}
